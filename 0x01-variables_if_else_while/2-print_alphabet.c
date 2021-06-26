0f09eb0f3fed85851830b3e253340e35b30bae49
ViCtOrSeT <victorkaluokoroonyeani@gmail.com>
2021-06-25 17:28:40 -0700

    ALPHABET GAME

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+{
+char abc;
+
+for (abc = 'a'; abc <= 'z'; abc++)
+{
+putchar(abc);
+}
+putchar('\n');
+return (0);
+}
