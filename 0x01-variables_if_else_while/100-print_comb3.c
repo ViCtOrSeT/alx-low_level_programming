3ca5a11a8fb6323aa8e33264dc35c5cde588cdc8
ViCtOrSeT <victorkaluokoroonyeani@gmail.com>
2021-07-01 11:06:34 -0700

    PRINT COMB 3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,27 +11,27 @@ for (n = '0'; n < '9'; n++)
 {
 for (n1 = '0'; n1 < '9'; n1++)
 {
-if (n == '0'; && n1 < '1')
+if (n == '0' && n1 < '1')
 continue;
-if (n == '1'; && n1 < '2')
+if (n == '1' && n1 < '2')
 continue;
-if (n == '2'; && n1 < '3')
+if (n == '2' && n1 < '3')
 continue;
-if (n == '3'; && n1 < '4')
+if (n == '3' && n1 < '4')
 continue;
-if (n == '4'; && n1 < '5')
+if (n == '4' && n1 < '5')
 continue;
-if (n == '5'; && n1 < '6')
+if (n == '5' && n1 < '6')
 continue;
-if (n == '6'; && n1 < '7')
+if (n == '6' && n1 < '7')
 continue;
-if (n == '7'; && n1 < '8')
+if (n == '7' && n1 < '8')
 continue;
-if (n == '8'; && n1 < '9')
+if (n == '8' && n1 < '9')
 continue;
 putchar(n);
 putchar(n1);
-if (n == '8'; && n1 < '9')
+if (n == '8' && n1 < '9')
 break;
 putchar(',');
 putchar(' ');
