d2d49f2ebdd86cea98283a9b678d173755f76675
ViCtOrSeT <victorkaluokoroonyeani@gmail.com>
2021-07-10 15:59:47 -0700

    PUTCHAR

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,14 +1,22 @@
-#include <holberton.h>
+#include "holberton.h"
 
 /**
- * main - Entry point
+ * main - print holberton
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-_putchar("Holberton");
-_putchar("\n");
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
 return (0);
 }
