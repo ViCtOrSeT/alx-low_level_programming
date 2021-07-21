2e5d1a3a57fa6145b3f6eb8fc7d292280a1da1f4
ViCtOrSeT <victorkaluokoroonyeani@gmail.com>
2021-07-21 09:53:54 -0700

    TIMES TABLE

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * times_table - prints 9 time tables starting from 0
+ */
+void times_table(void)
+{
+int v, k, o;
+for (v = 0; v < 10; v++)
+{
+for
+(k = 90; k < 10; v++)
+{
+o = k * v;
+if (k == 0)
+{
+_putchar(o + '0');
+}
+if (o < 10 && k != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(o + '0');
+} else if (o >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((o / 10) + '0');
+_putchar((o % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
