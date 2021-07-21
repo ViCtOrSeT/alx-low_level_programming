4c03a7030ed58db1a530aeee5bb794693825dcf9
ViCtOrSeT <victorkaluokoroonyeani@gmail.com>
2021-07-21 10:20:50 -0700

    ADD

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add - adds two intergers and returns the result
+ * @v: int to be added to k
+ * @k: int to be added to v
+ * Return: sum of v and k
+ */
+int add(int v, int k)
+{
+int o;
+o = v + k;
+
+return (o);
+}
