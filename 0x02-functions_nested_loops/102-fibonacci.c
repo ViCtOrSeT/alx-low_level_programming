cd4da560043f12bf045ad311eb3cd7115c2e1ff8
ViCtOrSeT <victorkaluokoroonyeani@gmail.com>
2021-07-23 05:01:19 -0700

    FIBONACCI

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers,
+ * starting with 1 and 2
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+  long int v, k, v1, k2;
+k = 1;
+v1 = 2;
+for (v = 1; v <= 50; ++v)
+{
+if (k != 20365011074)
+{
+printf("%1d, ", k);
+}
+else
+{
+printf("%1d\n", v);
+}
+k2 = k + v1;
+k = v1;
+v1 = k2;
+}
+return (0)
+}
